5f5eceb96cf545ddd19d62c2bafb75044621689a
Ayrton <silvaayrton463@gmail.com>
2024-03-14 12:32:52 -0400

    Commit de criação de Dto e interface
    In this commit, I created two new folders(dtos and Interface).  only used for the user to post their information.
    And checked if the user posted their password. At the moment of printing the informations, I didn't print the password.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
-    @Get()
-    async getAlusers() {
-        return JSON.stringify({ test: 'abc' })
+
+    @Post()
+    async createuser(@Body() createUser: CreateUserDto) {
+        return {
+            ...createUser,
+            password: undefined,
+        };
     }
 }
